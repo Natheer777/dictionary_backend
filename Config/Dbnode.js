cfef533fe4de1f40f30d9c6e73dc29e7ee2fbb20
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2024-07-04 07:23:58 +0300

    s

--- Config/Dbnode.js
+++ Config/Dbnode.js
@@ -12,11 +12,7 @@
 // })
 
 // module.exports=db;
-const express = require('express');
 const mysql = require('mysql2');
-const app = express();
-
-// انشاء الاتصال بقاعدة البيانات
 const db = mysql.createPool({
   host: 'betejqbmtak0eaj63ctn-mysql.services.clever-cloud.com',
   user: 'ufeuignwbwarfssi',
@@ -25,7 +21,6 @@ const db = mysql.createPool({
   database: 'betejqbmtak0eaj63ctn'
 });
 
-// تأكيد الاتصال
 db.getConnection((err, connection) => {
   if (err) {
     console.error('خطأ في الاتصال بقاعدة البيانات:', err);
@@ -35,12 +30,8 @@ db.getConnection((err, connection) => {
   connection.release();
 });
 
-// تعريف الأمور الأخرى في التطبيق Express
-// مثل إعداد المسارات، الوسيطة، وغيرها
-
-// كود التحديث كل ثانية
 function updateDataEverySecond() {
-  const query = "UPDATE words = REPLACE(REPLACE(REPLACE(REPLACE(writings, '[', ''), ']', ''), '\"', ''), '\\\\', '')";
+  const query = "UPDATE words SET writings = REPLACE(REPLACE(REPLACE(REPLACE(writings, '[', ''), ']', ''), '\"', ''), '\\\\', '')";
   setInterval(() => {
     db.query(query, (err, results) => {
       if (err) {
@@ -52,17 +43,6 @@ function updateDataEverySecond() {
   }, 1000); // كل ثانية
 }
 
-// استدعاء الدالة لبدء تحديث البيانات كل ثانية
 updateDataEverySecond();
 
-// إضافة أي مسارات أو منطق إضافي لتطبيق Express
-// مثل:
-// app.get('/', (req, res) => {
-//   res.send('مرحباً بك في تطبيق Express');
-// });
-
-// استمع على منفذ محدد
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`التطبيق يعمل على المنفذ ${port}`);
-});
+module.exports = db;
