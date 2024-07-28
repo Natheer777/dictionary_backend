f5a5cb86e17ce798428c19a5bc03d416b1492bc9
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2024-07-28 05:17:27 +0300

    ss

--- Config/Dbnode.js
+++ Config/Dbnode.js
@@ -1,47 +1,14 @@
-
-// const mysql = require('mysql2');
-// const db = mysql.createPool({
-//   host: 'betejqbmtak0eaj63ctn-mysql.services.clever-cloud.com',
-//   user: 'ufeuignwbwarfssi',
-//   port: '3306',
-//   password: 'Te1G6CP4sznNaOGqQypN',
-//   database: 'betejqbmtak0eaj63ctn'
-// });
-
-// db.getConnection((err, connection) => {
-//   if (err) {
-//     console.error('خطأ في الاتصال بقاعدة البيانات:', err);
-//     return;
-//   }
-//   console.log('تم الاتصال بقاعدة البيانات بنجاح');
-//   connection.release();
-// });
-
-// function updateDataEverySecond() {
-//   const query = "UPDATE words SET writings = REPLACE(REPLACE(REPLACE(REPLACE(writings, '[', ''), ']', ''), '\"', ''), '\\\\', '')";
-//   setInterval(() => {
-//     db.query(query, (err, results) => {
-//       if (err) {
-//         console.error('خطأ في تحديث البيانات:', err);
-//         return;
-//       }
-//       console.log('تم تحديث البيانات بنجاح');
-//     });
-//   }, 1000); // كل ثانية
-// }
-
-// updateDataEverySecond();
-
-// module.exports = db;
-
-
 const mysql = require('mysql2');
 const db = mysql.createPool({
-  host: 'betejqbmtak0eaj63ctn-mysql.services.clever-cloud.com',
-  user: 'ufeuignwbwarfssi',
-  port: '3306',
-  password: 'Te1G6CP4sznNaOGqQypN',
-  database: 'betejqbmtak0eaj63ctn'
+  host: 'srv621.hstgr.io',
+  user: 'u229294786_UKHrI',
+  password: 'Sawa2020!',
+  database: 'u229294786_JZfqq'
+  // host: 'betejqbmtak0eaj63ctn-mysql.services.clever-cloud.com',
+  // user: 'ufeuignwbwarfssi',
+  // port: '3306',
+  // password: 'Te1G6CP4sznNaOGqQypN',
+  // database: 'betejqbmtak0eaj63ctn'
 });
 
 db.getConnection((err, connection) => {
